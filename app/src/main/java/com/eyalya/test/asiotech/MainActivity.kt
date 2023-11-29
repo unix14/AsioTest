2ebd0d8e6aded3ec1840a4875841a5a7ef012ff6
Eyal Yaakobi <unix14@gmail.com>
2023-11-29 23:51:43 +0200

    add fragments boilerplate code

--- app/src/main/java/com/eyalya/test/asiotech/MainActivity.kt
+++ app/src/main/java/com/eyalya/test/asiotech/MainActivity.kt
@@ -3,16 +3,22 @@ package com.eyalya.test.asiotech
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.eyalya.test.asiotech.databinding.ActivityMainBinding
+import com.eyalya.test.asiotech.ui.BagsListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadFragment(BagsListFragment())
     }
 
-    fun loadFragment(fragment: Fragment) {
-
+    fun loadFragment(fragment: Fragment) = with(binding){
+        supportFragmentManager.beginTransaction().add(fragment, fragment.tag).commit()
     }
 }
--- app/src/main/java/com/eyalya/test/asiotech/ui/BagsListFragment.kt
+++ app/src/main/java/com/eyalya/test/asiotech/ui/BagsListFragment.kt
@@ -1,19 +1,42 @@
 package com.eyalya.test.asiotech.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.eyalya.test.asiotech.databinding.FragmentBagsListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BagsListFragment: Fragment() {
 
+    private lateinit var binding: FragmentBagsListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBagsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initObservers()
+        initUi()
+    }
+
+    private fun initObservers() /*= with(viewModel) */{
+
+    }
+
+    private fun initUi() =with(binding) {
 
     }
 
